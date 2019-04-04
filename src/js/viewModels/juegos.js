714dc4fe43e4b2aecccc422b43f58de23f64f82f
AlexisTarjuelo <43894609+AlexisTarjuelo@users.noreply.github.com>
2019-04-04 13:32:08 +0200

    Update juegos.js

--- src/js/viewModels/juegos.js
+++ src/js/viewModels/juegos.js
@@ -5,7 +5,6 @@
  */
 /*
  * Your customer ViewModel code goes here
- prueba
  */
 define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-element-utils', 'ojs/ojselectcombobox'],
  function(oj, ko, $, app, moduleUtils) {
