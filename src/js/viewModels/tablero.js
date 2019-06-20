c45da790d8a56641101237f2d8eed6756b767b80
Cesarsanchezjerez <32263955+Cesarsanchezjerez@users.noreply.github.com>
2019-06-21 01:13:08 +0200

    botones completados
    añadido lo que falto al gandul de pedro

--- src/js/viewModels/tablero.js
+++ src/js/viewModels/tablero.js
@@ -8,7 +8,7 @@
  */
 define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-element-utils', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojlabel','ojs/ojselectcombobox'],
  function(oj, ko, $, app, moduleUtils) {
-  
+
     function TableroViewModel() {
       var self = this;
       self.userName=ko.observable(app.userName);
@@ -17,9 +17,18 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
 
       self.selectedGame=ko.observable("");
       self.palabras=ko.observableArray([]);
-      
-      
-      self.pal =ko.observable("");
+
+
+      self.palabra1 =ko.observable("");
+      self.palabra2 =ko.observable("");
+      self.palabra3 =ko.observable("");
+      self.palabra4 =ko.observable("");
+      self.palabra5 =ko.observable("");
+      self.palabra6 =ko.observable("");
+      self.palabra7 =ko.observable("");
+      self.palabra8 =ko.observable("");
+      self.palabra9 =ko.observable("");
+
       function loadPalabras(){
         if (app.userName!=null){
           var recurso="http://localhost:8080/getPalabras";
@@ -32,23 +41,23 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
             },
             success : showPalabras
           });
-        } 
+        }
       }
       //
       function showPalabras(respuesta){
         var palabras= respuesta.resultado.palabras;
-        pal="patata";
-        
-        
-        
-       
-        
+
+
+
+
+
+
       }
 
 
       self.onmessage=function(event){
         var data=JSON.parse(event.data);
-       
+
       }
 
 
@@ -64,29 +73,42 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
       self.handleActivated=function(info){
         self.userName=ko.observable(app.userName);
         self.opponentUserName=ko.observable(app.opponentUserName);
-        self.contenido=app.content;
-        self.tablero1=self.contenido.tablero[1];
-        self.pal=self.tablero1.tablero1[0];
-        
+
+
       }
-     
+      self.pruebaciones=function(){
+          self.contenido=app.content;
+          self.tablero1=self.contenido.tablero[1];
+          self.palabra1=self.tablero1.tablero1[0];
+          self.palabra2=self.tablero1.tablero1[1];
+          self.palabra3=self.tablero1.tablero1[2];
+          self.palabra4=self.tablero1.tablero1[3];
+          self.palabra5=self.tablero1.tablero1[4];
+          self.palabra6=self.tablero1.tablero1[5];
+          self.palabra7=self.tablero1.tablero1[6];
+          self.palabra8=self.tablero1.tablero1[7];
+          self.palabra9=self.tablero1.tablero1[8];
+
+      }
+
       self.connected = function() {
         var info =JSON.parse(sessionStorage.info);
         self.dealWithMessage(info);
         self.handleActivated(info);
+        self.pruebaciones();
       };
-    
+
 
     self.button1Text = "Button 1";
 
-    
+
     self.clickedButton = ko.observable("(None clicked yet)");
     self.buttonClick = function(event){
         self.clickedButton(event.currentTarget.id);
         return true;
     }
-  
-    
+
+
       /**
        * Optional ViewModel method invoked after the View is disconnected from the DOM.
        */
@@ -99,7 +121,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
       };
     }
 
-   
+
     return new TableroViewModel();
   }
 );
